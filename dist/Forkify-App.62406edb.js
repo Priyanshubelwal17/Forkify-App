0036e73bf4ffb8c2d9c39c230148a2ae36e51a8c
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-04-28 23:47:50 +0530

    Loading a recipe from api

--- dist/Forkify-App.62406edb.js
+++ dist/Forkify-App.62406edb.js
@@ -5,8 +5,32 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // NEW API URL (instead of the one shown in the video)
- // https://forkify-api.jonas.io
- ///////////////////////////////////////
+};
+// NEW API URL (instead of the one shown in the video)
+// https://forkify-api.jonas.io
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886');
+        const data = await res.json();
+        console.log(res, data);
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.sourceUrl,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.ingredients,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
 
 //# sourceMappingURL=Forkify-App.62406edb.js.map
